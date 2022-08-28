649055d31d1d6c3470ae23a4881292689a984414
noobExtendsBot <hackerearth02@gmail.com>
2022-08-28 15:09:39 +0530

    242 added a diff solution

--- leetcode/242_valid_anagram.py
+++ leetcode/242_valid_anagram.py
@@ -5,6 +5,8 @@ Problem Link: https://leetcode.com/problems/valid-anagram/
 
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        # Time Complexity: O(n)
+        # Space Complexity: O(200)
         map_s = [0 for i in range(200)]
         map_t = [0 for i in range(200)]
 
@@ -14,3 +16,16 @@ class Solution:
             map_t[ord(i)] += 1
 
         return map_s == map_t
+
+        # another solution without any extra space
+        # Time Complexity: O(n)
+        # Space Complexity: O(26)
+        # if len(s) != len(t):
+        #     return False
+        
+        # map_s, map_t = {}, {}
+        # for i in range(len(s)): 
+        #     map_s[s[i]] = 1 + map_s.get(s[i], 0)
+        #     map_t[t[i]] = 1+ map_t.get(t[i], 0)
+        
+        # return map_s == map_t
