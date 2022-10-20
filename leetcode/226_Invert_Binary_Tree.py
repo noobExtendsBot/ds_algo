f0a3601798c01de1b31e9711b9e832fddad4d850
noobExtendsBot <hackerearth02@gmail.com>
2022-10-20 12:23:15 +0100

    leetcode 226

---
+++ leetcode/226_Invert_Binary_Tree.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def postorderToInvert(self, c_node):
+        if c_node is None:
+            return None
+
+        self.postorderToInvert(c_node.left)
+        self.postorderToInvert(c_node.right)
+        # l, r = c_node.left, c_node.right
+        c_node.left, c_node.right = c_node.right, c_node.left
+
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if root is None:
+            return root
+
+        self.postorderToInvert(root)
+        return root
