0e889808f5da365a0adae28c999636fe2a7a5f28
noobExtendsBot <hackerearth02@gmail.com>
2023-06-02 18:52:29 +0100

    another solution m*n for Leetcode 49

--- leetcode/49_group_anagrams.py
+++ leetcode/49_group_anagrams.py
@@ -7,6 +7,7 @@ from collections import defaultdict
 
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # Time Complexity: nlogn * len(strs)
         if len(strs) == 0:
             return [[""]]
         
@@ -20,5 +21,21 @@ class Solution:
             res.append(v)
         # print(res)
         return res
+    
+    def groupAnagrams2(self, strs: List[str]) -> List[List[str]]:
+        # Another Solution, time complexity: m*n [m = len(strs); n = len(s)]
+        res = list()
+        hash_map = dict()
+        for s in strs:
+            temp = [0]*26
+            for i in s:
+                temp[ord(i)-97] += 1
+            # key = str(''.join(map(str, temp)))
+            try:
+                hash_map[tuple(temp)].append(s)
+            except KeyError:
+                hash_map[tuple(temp)] = list()
+                hash_map[tuple(temp)].append(s)
+        return hash_map.values()
         
 
