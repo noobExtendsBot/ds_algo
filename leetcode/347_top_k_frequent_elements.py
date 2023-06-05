e68c8c5fbdaf5d5204de3d210fd562a3be66056f
noobExtendsBot <hackerearth02@gmail.com>
2023-06-05 22:47:17 +0100

    LeetCode 347 added multiple methods

--- leetcode/347_top_k_frequent_elements.py
+++ leetcode/347_top_k_frequent_elements.py
@@ -4,6 +4,7 @@ Problem Link: https://leetcode.com/problems/top-k-frequent-elements/
 
 
 class Solution:
+    # Time Complexity: O(n)
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         res = list()
         count_map = dict()
@@ -21,4 +22,21 @@ class Solution:
                     break
                 res.append(j)
                 k -= 1
+        return res
+
+    def topKFrequent2(self, nums: List[int], k: int) -> List[int]:
+        # Time Complexity: nlogn
+        hash_map = dict()
+        res = list()
+        for i in nums:
+            hash_map[str(i)] = hash_map.get(str(i), 0) + 1
+        # print(hash_map)
+        hash_map = dict(sorted(hash_map.items(), key=lambda item: item[1], reverse=True))
+        # print(hash_map)
+        
+        for key, value in hash_map.items():
+            res.append(int(key))
+            k -= 1
+            if k == 0:
+                break
         return res
